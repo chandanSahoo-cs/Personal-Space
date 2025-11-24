7ff693ccffbf4d23a3cd2294efd427a464c8d561
Chandan <chandansahoo02468@gmail.com>
2025-11-24 21:13:17 +0530

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req: Request) {
+  console.log("Hello, I'm middleware");
   const allowedOrigins = [
     "http://localhost:3000",
     "https://personal-space-iota.vercel.app",
@@ -8,6 +9,9 @@ export function middleware(req: Request) {
 
   const origin = req.headers.get("origin");
   const auth = req.headers.get("authorization") || "";
+
+  console.log("Origin:", origin);
+  console.log("Auth:", auth);
 
   const isValidOrigin =
     allowedOrigins.includes(String(origin)) || origin === null;
